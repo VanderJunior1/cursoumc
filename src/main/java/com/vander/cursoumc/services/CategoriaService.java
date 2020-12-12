e7dae0607dfa5ec3c4bf5b1b79d72902f13b5bd8
Vander Junior <vanderjunior1@hotmail.com>
2020-12-12 11:52:56 -0300

    Ajuste no endpoint Categoria, criando exceptions personalizadas

--- src/main/java/com/vander/cursoumc/services/CategoriaService.java
+++ src/main/java/com/vander/cursoumc/services/CategoriaService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.vander.cursoumc.domain.Categoria;
 import com.vander.cursoumc.repositories.CategoriaRepository;
+import com.vander.cursoumc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -17,7 +18,8 @@ public class CategoriaService {
 	
 	public Categoria find(Integer id) {
 		 Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		 return obj.orElseThrow(() -> new ObjectNotFoundException(
+					"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 		} 
 	
 	public List<Categoria> findAll(){
