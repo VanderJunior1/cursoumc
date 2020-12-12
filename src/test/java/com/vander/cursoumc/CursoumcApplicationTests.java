8495601cfe7b23392981f4a4ea985dde6b4734b1
Vander Junior <vanderjunior1@hotmail.com>
2020-12-12 10:59:06 -0300

    Projeto criado

---
+++ src/test/java/com/vander/cursoumc/CursoumcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vander.cursoumc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoumcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
