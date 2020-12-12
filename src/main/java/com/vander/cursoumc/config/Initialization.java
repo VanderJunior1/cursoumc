4516f290b3cc1e9c8e936657601c8712c0389403
Vander Junior <vanderjunior1@hotmail.com>
2020-12-12 11:44:24 -0300

    Classe Produto e associacao muito para muitos

--- src/main/java/com/vander/cursoumc/config/Initialization.java
+++ src/main/java/com/vander/cursoumc/config/Initialization.java
@@ -1,5 +1,6 @@
 package com.vander.cursoumc.config;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,13 +8,17 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
 import com.vander.cursoumc.domain.Categoria;
+import com.vander.cursoumc.domain.Produto;
 import com.vander.cursoumc.repositories.CategoriaRepository;
+import com.vander.cursoumc.repositories.ProdutoRepository;
 
 @Configuration
 public class Initialization implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository cateRepo;
+	@Autowired
+	private ProdutoRepository prodRepo;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -22,9 +27,20 @@ public class Initialization implements CommandLineRunner {
 
 		Categoria c1 = new Categoria(null, "Informática");
 		Categoria c2 = new Categoria(null, "Escritório");
+		
+		Produto p1 = new Produto(null, "Computador", BigDecimal.valueOf(2000.00));
+		Produto p2 = new Produto(null, "Impressora", BigDecimal.valueOf(800.00));
+		Produto p3 = new Produto(null, "Mouse", BigDecimal.valueOf(80.00));
 
+		c1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		c2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(c1));
+		p2.getCategorias().addAll(Arrays.asList(c1,c2));
+		p3.getCategorias().addAll(Arrays.asList(c1));
+		
 		cateRepo.saveAll(Arrays.asList(c1, c2));
-
+		prodRepo.saveAll(Arrays.asList(p1,p2,p3));
 	}
 
 }
--- src/main/java/com/vander/cursoumc/domain/Produto.java
+++ src/main/java/com/vander/cursoumc/domain/Produto.java
@@ -1,6 +1,7 @@
 package com.vander.cursoumc.domain;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,7 +10,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -18,28 +23,35 @@ import lombok.Setter;
 @Getter @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
-public class Categoria implements Serializable{
+public class Produto  implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@EqualsAndHashCode.Include
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	
+
 	@Column(length = 100, nullable = false)
 	private String nome;
 	
-	@ManyToMany(mappedBy = "categorias")
-	private List<Produto> produtos = new ArrayList<>();
-
-
-	public Categoria() {
+	private BigDecimal preco;
+	
+	@JsonIgnore
+	@ManyToMany
+	@JoinTable(name = "PRODUTO_CATEGORIA",
+					joinColumns = @JoinColumn(name="produto_id"),
+					inverseJoinColumns = @JoinColumn(name="categoria_id"))
+	private List<Categoria> categorias = new ArrayList<>();
+	
+	public Produto() {
 	}
 
-	public Categoria(Integer id, String nome) {
+	public Produto(Integer id, String nome, BigDecimal preco) {
 		super();
 		this.id = id;
 		this.nome = nome;
+		this.preco = preco;
 	}
-
+	
+	
 }
