c3d2debdfebecf76bb51d6a75f4fbd79dd4bc7b3
Vander Junior <vanderjunior1@hotmail.com>
2020-12-12 12:02:04 -0300

    Criacao Estado e Cidade

--- src/main/java/com/vander/cursoumc/config/Initialization.java
+++ src/main/java/com/vander/cursoumc/config/Initialization.java
@@ -8,8 +8,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
 import com.vander.cursoumc.domain.Categoria;
+import com.vander.cursoumc.domain.Cidade;
+import com.vander.cursoumc.domain.Estado;
 import com.vander.cursoumc.domain.Produto;
 import com.vander.cursoumc.repositories.CategoriaRepository;
+import com.vander.cursoumc.repositories.CidadeRepository;
+import com.vander.cursoumc.repositories.EstadoRepository;
 import com.vander.cursoumc.repositories.ProdutoRepository;
 
 @Configuration
@@ -19,6 +23,10 @@ public class Initialization implements CommandLineRunner {
 	private CategoriaRepository cateRepo;
 	@Autowired
 	private ProdutoRepository prodRepo;
+	@Autowired
+	private EstadoRepository estadoRepo;
+	@Autowired
+	private CidadeRepository cidadeRepo;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -41,6 +49,19 @@ public class Initialization implements CommandLineRunner {
 		
 		cateRepo.saveAll(Arrays.asList(c1, c2));
 		prodRepo.saveAll(Arrays.asList(p1,p2,p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "são Paulo");
+		
+		Cidade cidade = new Cidade(null ,"Uberlândia", est1);
+		Cidade cidade2 = new Cidade(null ,"São Paulo", est2);
+		Cidade cidade3 = new Cidade(null ,"campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(cidade));
+		est2.getCidades().addAll(Arrays.asList(cidade2,cidade3));
+		
+		estadoRepo.saveAll(Arrays.asList(est1,est2));
+		cidadeRepo.saveAll(Arrays.asList(cidade,cidade2,cidade3));
 	}
 
 }
---
+++ src/main/java/com/vander/cursoumc/domain/Estado.java
@@ -0,0 +1,48 @@
+package com.vander.cursoumc.domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Estado implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(length = 100, nullable = false)
+	private String nome;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "estado")
+	private List<Cidade> cidades = new ArrayList<>();
+
+
+	public Estado() {
+	}
+
+	public Estado(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+}
