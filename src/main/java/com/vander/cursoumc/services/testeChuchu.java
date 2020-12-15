8cce7d21e45cc3ea2dd52daca542925b03799097
Vander Junior <vanderjunior1@hotmail.com>
2020-12-15 07:56:26 -0300

    teste bigdecimal

---
+++ src/main/java/com/vander/cursoumc/services/testeChuchu.java
@@ -0,0 +1,14 @@
+package com.vander.cursoumc.services;
+
+import java.math.BigDecimal;
+
+public class testeChuchu {
+	
+	public static void main(String[] args) {
+		BigDecimal soma  = BigDecimal.ONE;
+//		soma.add(BigDecimal.valueOf(50);
+		
+		System.out.println(soma);
+	}
+
+}
